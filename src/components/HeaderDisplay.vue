b628f196b99c9df236ba0a1d9300f7faf19dbe47
wangkodok <wangkodok@gmail.com>
2022-07-15 16:22:52 +0900

    chapter_57: 날씨앱-템플릿

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h2>Weather Application</h2>
+    <p>{{ username }}님 반갑습니다.</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderDisplay",
+};
+</script>
+
+<style scoped>
+.header {
+  background: linear-gradient(15deg, #ed4264, #ffedbc);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+}
+.header h2 {
+  color: #fff;
+}
+.header p {
+  font-size: 12px;
+  color: #fff;
+}
+</style>
---
+++ src/components/weather/WeatherContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weather-wrapper">
+    <ContentHeader />
+    <CitySelector />
+  </div>
+</template>
+
+<script>
+import ContentHeader from "./ContentHeader.vue";
+import CitySelector from "./CitySelector.vue";
+export default {
+  name: "WeatherContent",
+  components: {
+    ContentHeader,
+    CitySelector,
+  },
+};
+</script>
+
+<style></style>
